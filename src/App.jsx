58ef4d887d1aa00c6d909dbc3951b0fb794d7f1c
Kannugithub <105802306+kannugithub@users.noreply.github.com>
2026-03-03 14:28:46 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NutritionMeter from "./components/Nutrition/NutritionMeter";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <NutritionMeter />
+    </div>
+  );
+}
+
+export default App;
